21b0d45125eb0475c3210ca4cdb7ef6826a2ddf2
Vlads Sokolovs <118102879+5oK0L@users.noreply.github.com>
2023-03-05 17:14:54 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,28 +1,49 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.children = []
+# python3
 
-def compute_height(node):
-    if not node.children:
+import sys
+import threading
+import numpy
+
+
+def compute_height(n, parents):
+    tree = {}
+    root = None
+    for i in range(n):
+        parent = parents[i]
+        if parent == -1:
+            root = i
+        else:
+            if parent not in tree:
+                tree[parent] = []
+            tree[parent].append(i)
+    return get_height(tree, root)
+
+def get_height(tree, root):
+    if root not in tree:
         return 1
-    heights = []
-    for child in node.children:
-        heights.append(compute_height(child))
-    return 1 + max(heights)
+    max_height = 0
+    for child in tree[root]:
+        height = get_height(tree, child)
+        max_height = max(max_height, height)
+    return max_height + 1
 
-n = int(input())
-parents = list(map(int, input().split()))
 
-# Create a list of nodes with values 0 to n-1
-nodes = [Node(i) for i in range(n)]
+def main():
+    # read input
+    n = int(input())
+    parents = list(map(int, input().split()))
 
-# Connect child nodes to their parent nodes
-for i, parent in enumerate(parents):
-    if parent == -1:
-        root = nodes[i]
-    else:
-        nodes[parent].children.append(nodes[i])
+    # compute height of the tree
+    height = compute_height(n, parents)
 
-# Compute and output the height of the tree
-print(compute_height(root))
+    # write output
+    print(height)
+
+# In Python, the default limit on recursion depth is rather low,
+# so raise it here for this problem. Note that to take advantage
+# of bigger stack, we have to launch the computation in a new thread.
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
+threading.Thread(target=main).start()
+main()
+# print(numpy.array([1,2,3]))
